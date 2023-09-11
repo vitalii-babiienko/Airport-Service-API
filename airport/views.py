acdf19fd349220aaaf2684e164e53a3a8920214c
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-09-11 17:59:43 +0300

    Implement pagination feature

--- airport/views.py
+++ airport/views.py
@@ -12,6 +12,14 @@ from airport.models import (
     Flight,
     Order,
 )
+from airport.pagination import (
+    AirplanePagination,
+    CrewPagination,
+    AirportPagination,
+    OrderPagination,
+    RoutePagination,
+    FlightPagination,
+)
 from airport.serializers import (
     AirplaneTypeSerializer,
     AirplaneSerializer,
@@ -39,6 +47,7 @@ class AirplaneTypeViewSet(viewsets.ModelViewSet):
 class AirplaneViewSet(viewsets.ModelViewSet):
     queryset = Airplane.objects.select_related("airplane_type")
     serializer_class = AirplaneSerializer
+    pagination_class = AirplanePagination
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = ("airplane_type",)
 
@@ -55,6 +64,7 @@ class AirplaneViewSet(viewsets.ModelViewSet):
 class CrewViewSet(viewsets.ModelViewSet):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
+    pagination_class = CrewPagination
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = ("position",)
 
@@ -62,6 +72,7 @@ class CrewViewSet(viewsets.ModelViewSet):
 class AirportViewSet(viewsets.ModelViewSet):
     queryset = Airport.objects.all()
     serializer_class = AirportSerializer
+    pagination_class = AirportPagination
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = ("city", "country")
 
@@ -75,6 +86,7 @@ class AirportViewSet(viewsets.ModelViewSet):
 class RouteViewSet(viewsets.ModelViewSet):
     queryset = Route.objects.select_related("source", "destination")
     serializer_class = RouteSerializer
+    pagination_class = RoutePagination
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = ("source", "destination")
 
@@ -91,7 +103,11 @@ class RouteViewSet(viewsets.ModelViewSet):
 class FlightViewSet(viewsets.ModelViewSet):
     queryset = (
         Flight.objects
-        .select_related("route", "airplane")
+        .select_related(
+            "route__source",
+            "route__destination",
+            "airplane__airplane_type",
+        )
         .prefetch_related("crews")
         .annotate(
             tickets_available=(
@@ -101,6 +117,7 @@ class FlightViewSet(viewsets.ModelViewSet):
         )
     )
     serializer_class = FlightSerializer
+    pagination_class = FlightPagination
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = ("route", "departure_time", "arrival_time")
 
@@ -115,8 +132,13 @@ class FlightViewSet(viewsets.ModelViewSet):
 
 
 class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all()
+    queryset = Order.objects.prefetch_related(
+        "tickets__flight__airplane",
+        "tickets__flight__crews",
+        "tickets__flight__route",
+    )
     serializer_class = OrderSerializer
+    pagination_class = OrderPagination
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = ("created_at",)
 
